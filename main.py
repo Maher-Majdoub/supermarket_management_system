f4b8ebd95a37d1a00ed3c7a45700116af4423265
Maher Majdoub <maher.majjdoub@gmail.com>
2022-11-03 13:04:45 -0700

    mofidified the main.py file

--- main.py
+++ main.py
@@ -8,19 +8,50 @@ from PyQt5 import QtCore, QtGui, QtWidgets, uic
 import sys
 
 
-class Ui(QtWidgets.QMainWindow):
+class Ui(QtWidgets.QWidget):
     def __init__(self):
-        super(Ui, self).__init__()
+        QtWidgets.QWidget.__init__(self)
+        uic.loadUi('.\\gui\\admin.ui', self)
+        self.show()
+
+class LOGIN(QtWidgets.QWidget):
+    switch_window = QtCore.pyqtSignal()
+    def __init__(self):
+        QtWidgets.QWidget.__init__(self)
         self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
         uic.loadUi('.\\gui\\login_form.ui', self)
-        self.show()
+        self.login_btn.clicked.connect(self.login)
 
+    def login(self):
+        self.switch_window.emit()
+
+
+
+class Controller:
+    def __init__(self):
+        pass
+
+        
+    def show_login(self):
+
+        self.login = LOGIN()
+        self.login.switch_window.connect(self.show_admin_panel)
+        self.login.show()
+
+
+    def show_admin_panel(self):
+        self.admin_panel = Ui()
+        self.login.close()
+        self.admin_panel.show()
+        
+
+
+    
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    window = Ui()
-    window.close_btn.clicked.connect(window.close)
-    window.minimize_btn.clicked.connect(window.showMinimized)
-    app.exec_()
+    controller = Controller()
+    controller.show_login()
+    sys.exit(app.exec_())
